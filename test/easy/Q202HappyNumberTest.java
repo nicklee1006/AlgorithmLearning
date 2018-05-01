5d11eec9e2264ea778e1e2d83a0e665041b4e8ef
nick lee <nicklee1006@gmail.com>
2018-05-01 23:12:39 +1000

    add Question 202: Happy Number

---
+++ test/easy/Q202HappyNumberTest.java
@@ -0,0 +1,14 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q202HappyNumberTest {
+
+    @Test
+    public void isHappy() {
+        Assert.assertTrue(Q202HappyNumber.isHappy(19));
+    }
+}

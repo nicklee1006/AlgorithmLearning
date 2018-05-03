abfd692df7bdf57a2b5b3ad74344203b0f6ecfbc
nick lee <nicklee1006@gmail.com>
2018-05-03 20:43:24 +0930

    add Question 204: Count Primes

---
+++ test/easy/Q204CountPrimesTest.java
@@ -0,0 +1,14 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q204CountPrimesTest {
+
+    @Test
+    public void countPrimes() {
+        Assert.assertEquals(4, Q204CountPrimes.countPrimes(10));
+    }
+}

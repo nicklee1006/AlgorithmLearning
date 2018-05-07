16ebf18d2ee5f388a1011401c5b62ab21acfcc0d
nick lee <nicklee1006@gmail.com>
2018-05-07 15:47:05 +0930

    add Question 226. Invert Binary Tree

---
+++ src/easy/Q226InvertBinaryTree.java
@@ -0,0 +1,43 @@
+package easy;
+
+import util.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Question 226. Invert Binary Tree
+ * https://leetcode.com/problems/invert-binary-tree/description/
+ *
+ * Invert a binary tree.
+ */
+public class Q226InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+
+            // swap the left and right child
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+
+            // push to queue
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+
+        return root;
+    }
+}

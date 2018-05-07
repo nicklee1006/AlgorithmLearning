89dc987a5a778004b63e9ed0dc6ab1c0e9f86d90
nick lee <nicklee1006@gmail.com>
2018-05-07 15:08:36 +0930

    add Question 225. Implement Stack using Queues

---
+++ test/easy/Q225ImplementStackUsingQueuesTest.java
@@ -0,0 +1,45 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q225ImplementStackUsingQueuesTest {
+    private Q225ImplementStackUsingQueues stack;
+
+    @Before
+    public void setUp() throws Exception {
+        stack = new Q225ImplementStackUsingQueues();
+    }
+
+    @Test
+    public void push() {
+        stack.push(1);
+        stack.push(2);
+
+        Assert.assertFalse(stack.empty());
+    }
+
+    @Test
+    public void pop() {
+        stack.push(1);
+        stack.push(2);
+
+        Assert.assertEquals(2, stack.pop());
+    }
+
+    @Test
+    public void top() {
+        stack.push(1);
+        stack.push(2);
+
+        Assert.assertEquals(2, stack.top());
+    }
+
+    @Test
+    public void empty() {
+        Assert.assertTrue(stack.empty());
+    }
+}

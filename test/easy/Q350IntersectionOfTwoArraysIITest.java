29b7b00b61dfd231d90df09b62f9c020d807c9de
nick lee <nicklee1006@gmail.com>
2018-05-18 17:37:47 +0930

    add Question 350. Intersection of Two Arrays II

---
+++ test/easy/Q350IntersectionOfTwoArraysIITest.java
@@ -0,0 +1,15 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q350IntersectionOfTwoArraysIITest {
+
+    @Test
+    public void intersect() {
+//        Assert.assertArrayEquals(new int[]{2, 2}, Q350IntersectionOfTwoArraysII.intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2}));
+        Assert.assertArrayEquals(new int[]{1}, Q350IntersectionOfTwoArraysII.intersect(new int[]{1}, new int[]{1}));
+    }
+}

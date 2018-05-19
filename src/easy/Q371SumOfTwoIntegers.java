91ad5b79cddc74d69d9de2ec67460b1dd5a422b9
nick lee <nicklee1006@gmail.com>
2018-05-19 22:49:14 +0930

    add Question 371. Sum of Two Integers

---
+++ src/easy/Q371SumOfTwoIntegers.java
@@ -0,0 +1,15 @@
+package easy;
+
+/**
+ * 2018.5.19 Question 371. Sum of Two Integers
+ * https://leetcode.com/problems/sum-of-two-integers/description/
+ *
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ *
+ * hint: https://leetcode.com/problems/sum-of-two-integers/discuss/84278/A-summary:-how-to-use-bit-manipulation-to-solve-problems-easily-and-efficiently
+ */
+public class Q371SumOfTwoIntegers {
+    public static int getSum(int a, int b) {
+        return Math.addExact(a, b);
+    }
+}
---
+++ test/easy/Q371SumOfTwoIntegersTest.java
@@ -0,0 +1,14 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q371SumOfTwoIntegersTest {
+
+    @Test
+    public void getSum() {
+        Assert.assertEquals(3, Q371SumOfTwoIntegers.getSum(1,2));
+    }
+}

560d40be899f40e25f11c948dafa6954659d4f8e
nick lee <nicklee1006@gmail.com>
2018-05-29 19:44:34 +0930

    add Question 455. Assign Cookies

---
+++ test/easy/Q455AssignCookiesTest.java
@@ -0,0 +1,15 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q455AssignCookiesTest {
+
+    @Test
+    public void findContentChildren() {
+        Assert.assertEquals(1, Q455AssignCookies.findContentChildren(new int[]{1, 2, 3}, new int[]{1, 1}));
+        Assert.assertEquals(2, Q455AssignCookies.findContentChildren(new int[]{1, 2}, new int[]{1, 2, 3}));
+    }
+}

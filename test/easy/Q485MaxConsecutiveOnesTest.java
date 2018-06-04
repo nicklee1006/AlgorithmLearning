a3a7e504b82b70b0ccda188faa50da6fe38550f4
nick lee <nicklee1006@gmail.com>
2018-06-04 15:46:35 +0930

    add Question 485. Max Consecutive Ones

---
+++ test/easy/Q485MaxConsecutiveOnesTest.java
@@ -0,0 +1,16 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q485MaxConsecutiveOnesTest {
+
+    @Test
+    public void findMaxConsecutiveOnes() {
+        Assert.assertEquals(3, Q485MaxConsecutiveOnes.findMaxConsecutiveOnes(new int[]{1,1,0,1,1,1}));
+        Assert.assertEquals(6, Q485MaxConsecutiveOnes.findMaxConsecutiveOnes(new int[]{1,1,1,1,1,1}));
+        Assert.assertEquals(0, Q485MaxConsecutiveOnes.findMaxConsecutiveOnes(new int[]{0,0,0,0,0,0}));
+    }
+}

ac4d8bb41094c41f155b9c6f317e7be9d4c6dcca
nick lee <nicklee1006@gmail.com>
2018-06-05 20:29:32 +0930

    add Question 500. Keyboard Row

---
+++ src/easy/Q500KeyboardRow.java
@@ -0,0 +1,42 @@
+package easy;
+
+import java.util.*;
+
+/**
+ * 2018.6.5 Question 500. Keyboard Row
+ * https://leetcode.com/problems/keyboard-row/description/
+ *
+ * Given a List of words, return the words that can be typed using letters of alphabet on only one row's of
+ * American keyboard like the image below.
+ */
+public class Q500KeyboardRow {
+    public static String[] findWords(String[] words) {
+        String[] lines = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+        Map<Character, Integer> index = new HashMap<>();
+
+        for (int i = 0; i < lines.length; i++) {
+            for (char c : lines[i].toCharArray()) {
+                index.put(c, i);
+            }
+        }
+
+        List<String> result = new ArrayList<>();
+        for (String temp : words) {
+            int tempIndex = index.get(temp.toLowerCase().charAt(0));
+            for (char c : temp.toLowerCase().toCharArray()) {
+                int index2 = index.get(c);
+
+                if (index2 != tempIndex) {
+                    tempIndex = -1;
+                    break;
+                }
+            }
+
+            if (tempIndex != -1) {
+                result.add(temp);
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+}

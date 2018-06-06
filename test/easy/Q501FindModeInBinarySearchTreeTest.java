e3d2bac7c54e584a1c717460f4ee6ca42ac8128a
nick lee <nicklee1006@gmail.com>
2018-06-06 12:08:39 +0930

    add Question 501. Find Mode in Binary Search Tree

---
+++ test/easy/Q501FindModeInBinarySearchTreeTest.java
@@ -0,0 +1,12 @@
+package easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q501FindModeInBinarySearchTreeTest {
+
+    @Test
+    public void findMode() {
+    }
+}

406552f1ef2731be5a642759c22c84415b1c6f12
nick lee <nicklee1006@gmail.com>
2018-06-11 12:28:04 +0930

    add Question 543. Diameter of Binary Tree

---
+++ test/easy/Q543DiameterOfBinaryTreeTest.java
@@ -0,0 +1,12 @@
+package easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q543DiameterOfBinaryTreeTest {
+
+    @Test
+    public void diameterOfBinaryTree() {
+    }
+}

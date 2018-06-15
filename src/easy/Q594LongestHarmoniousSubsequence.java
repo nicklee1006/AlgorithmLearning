2d62831e1ffd33e26e295dce2e8bdb17529b1029
nick lee <nicklee1006@gmail.com>
2018-06-15 19:35:04 +0930

    add Question 594. Longest Harmonious Subsequence

---
+++ src/easy/Q594LongestHarmoniousSubsequence.java
@@ -0,0 +1,34 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 2018.6.15 Question 594. Longest Harmonious Subsequence
+ * https://leetcode.com/problems/longest-harmonious-subsequence/description/
+ *
+ * We define a harmonious array is an array where the difference between its maximum value and its minimum
+ * value is exactly 1.
+ *
+ * Now, given an integer array, you need to find the length of its longest harmonious subsequence among all
+ * its possible subsequences.
+ */
+public class Q594LongestHarmoniousSubsequence {
+    public static int findLHS(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int temp : nums) {
+            count.put(temp, count.getOrDefault(temp, 0) + 1);
+        }
+
+        int result = 0;
+
+        for (int key : count.keySet()) {
+            if (count.containsKey(key + 1)) {
+                result = Math.max(result, count.get(key) + count.get(key + 1));
+            }
+        }
+
+        return result;
+    }
+}

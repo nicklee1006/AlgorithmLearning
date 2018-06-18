27ecebed4ca5ef008094b2026b3ae4e9a649734b
nick lee <nicklee1006@gmail.com>
2018-06-18 13:56:02 +0930

    add Question 606. Construct String from Binary Tree

---
+++ test/easy/Q606ConstructStringFromBinaryTreeTest.java
@@ -0,0 +1,12 @@
+package easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q606ConstructStringFromBinaryTreeTest {
+
+    @Test
+    public void tree2str() {
+    }
+}

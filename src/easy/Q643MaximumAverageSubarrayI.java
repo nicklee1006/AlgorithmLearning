c904a10f8f0d0da81bf2dae6f6abd5f088aa8bd4
nick lee <nicklee1006@gmail.com>
2018-06-21 21:05:34 +0930

    add Question 643. Maximum Average Subarray I

---
+++ src/easy/Q643MaximumAverageSubarrayI.java
@@ -0,0 +1,29 @@
+package easy;
+
+/**
+ * 2018.6.21 Question 643. Maximum Average Subarray I
+ * https://leetcode.com/problems/maximum-average-subarray-i/description/
+ *
+ * Given an array consisting of n integers, find the contiguous subarray of given length k that has the
+ * maximum average value. And you need to output the maximum average value.
+ */
+public class Q643MaximumAverageSubarrayI {
+    public static double findMaxAverage(int[] nums, int k) {
+        int result = 0;
+
+        // initial
+        for (int i = 0; i < k; i++) {
+            result += nums[i];
+        }
+
+        int prev = result;
+        for (int i = k; i < nums.length; i++) {
+            int temp = prev - nums[i - k] + nums[i];
+
+            result = Math.max(result, temp);
+            prev = temp;
+        }
+
+        return result * 1.0 / k;
+    }
+}

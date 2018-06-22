491a8b8ad10d38794de62ef15453661d994c3c5e
nick lee <nicklee1006@gmail.com>
2018-06-22 18:34:28 +0930

    add Question 653. Two Sum IV - Input is a BST

---
+++ src/easy/Q653TwoSumIVInputIsABST.java
@@ -0,0 +1,35 @@
+package easy;
+
+import util.TreeNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 2018.6.22 Question 653. Two Sum IV - Input is a BST
+ * https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+ *
+ * Given a Binary Search Tree and a target number, return true if there exist two elements in the BST
+ * such that their sum is equal to the given target.
+ */
+public class Q653TwoSumIVInputIsABST {
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> numbers = new HashSet<>();
+
+        return dfs(root, numbers, k);
+    }
+
+    private boolean dfs(TreeNode node, Set set, int k) {
+        if (node == null) {
+            return false;
+        }
+
+        if (set.contains(k - node.val)) {
+            return true;
+        }
+
+        set.add(node.val);
+
+        return dfs(node.left, set, k) || dfs(node.right, set, k);
+    }
+}

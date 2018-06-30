d4ec7569dc8e9b0c459f17824a043a3c7381bc3b
nick lee <nicklee1006@gmail.com>
2018-06-30 22:15:15 +0930

    add Question 690. Employee Importance

---
+++ src/easy/Q690EmployeeImportance.java
@@ -0,0 +1,46 @@
+package easy;
+
+import util.Employee;
+
+import java.util.*;
+
+/**
+ * 2018.6.30 Question 690. Employee Importance
+ * https://leetcode.com/problems/employee-importance/description/
+ *
+ * You are given a data structure of employee information, which includes the employee's unique id, his
+ * importance value and his direct subordinates' id.
+ *
+ * For example, employee 1 is the leader of employee 2, and employee 2 is the leader of employee 3. They
+ * have importance value 15, 10 and 5, respectively. Then employee 1 has a data structure like [1, 15,
+ * [2]], and employee 2 has [2, 10, [3]], and employee 3 has [3, 5, []]. Note that although employee 3
+ * is also a subordinate of employee 1, the relationship is not direct.
+ *
+ * Now given the employee information of a company, and an employee id, you need to return the total
+ * importance value of this employee and all his subordinates.
+ */
+public class Q690EmployeeImportance {
+    public int getImportance(List<Employee> employees, int id) {
+        int result = 0;
+
+        Map<Integer, Employee> map = new HashMap<>();
+        for (Employee employee : employees) {
+            map.put(employee.id, employee);
+        }
+
+        Queue<Employee> queue = new LinkedList<>();
+        queue.add(map.get(id));
+
+        while (!queue.isEmpty()) {
+            Employee current = queue.poll();
+
+            result += current.importance;
+
+            for (int subId : current.subordinates) {
+                queue.add(map.get(subId));
+            }
+        }
+
+        return result;
+    }
+}
---
+++ test/easy/Q690EmployeeImportanceTest.java
@@ -0,0 +1,12 @@
+package easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q690EmployeeImportanceTest {
+
+    @Test
+    public void getImportance() {
+    }
+}

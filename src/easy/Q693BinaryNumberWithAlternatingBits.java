3493e1b2e04417dc688701d48881fdf1472fbde3
nick lee <nicklee1006@gmail.com>
2018-07-01 17:01:10 +0930

    add Question 693. Binary Number with Alternating Bits

---
+++ src/easy/Q693BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,22 @@
+package easy;
+
+/**
+ * 2018.7.1 Question 693. Binary Number with Alternating Bits
+ * https://leetcode.com/problems/binary-number-with-alternating-bits/description/
+ *
+ * Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will
+ * always have different values.
+ */
+public class Q693BinaryNumberWithAlternatingBits {
+    public static boolean hasAlternatingBits(int n) {
+        String binary = Integer.toBinaryString(n);
+
+        for (int i = 1; i < binary.length(); i++) {
+            if (binary.charAt(i - 1) == binary.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ test/easy/Q693BinaryNumberWithAlternatingBitsTest.java
@@ -0,0 +1,18 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q693BinaryNumberWithAlternatingBitsTest {
+
+    @Test
+    public void hasAlternatingBits() {
+        Assert.assertTrue(Q693BinaryNumberWithAlternatingBits.hasAlternatingBits(5));
+        Assert.assertTrue(Q693BinaryNumberWithAlternatingBits.hasAlternatingBits(10));
+        Assert.assertFalse(Q693BinaryNumberWithAlternatingBits.hasAlternatingBits(7));
+        Assert.assertFalse(Q693BinaryNumberWithAlternatingBits.hasAlternatingBits(11));
+
+    }
+}

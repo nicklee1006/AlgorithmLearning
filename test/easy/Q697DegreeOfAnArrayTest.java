76657a700d700703fe8728cdbc4f8dc5e635d539
nick lee <nicklee1006@gmail.com>
2018-07-02 22:24:15 +0930

    add Question 697. Degree of an Array

---
+++ test/easy/Q697DegreeOfAnArrayTest.java
@@ -0,0 +1,15 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q697DegreeOfAnArrayTest {
+
+    @Test
+    public void findShortestSubArray() {
+        Assert.assertEquals(2, Q697DegreeOfAnArray.findShortestSubArray(new int[]{1, 2, 2, 3, 1}));
+        Assert.assertEquals(6, Q697DegreeOfAnArray.findShortestSubArray(new int[]{1, 2, 2, 3, 1, 4, 2}));
+    }
+}

504bab8e13c53bfa54582c5811594bf2a8c88e91
nick lee <nicklee1006@gmail.com>
2018-07-03 14:06:57 +0930

    add Question 717. 1-bit and 2-bit Characters

---
+++ test/easy/Q7171bitAnd2bitCharactersTest.java
@@ -0,0 +1,15 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q7171bitAnd2bitCharactersTest {
+
+    @Test
+    public void isOneBitCharacter() {
+        Assert.assertTrue(Q7171bitAnd2bitCharacters.isOneBitCharacter(new int[]{1, 0, 0}));
+        Assert.assertFalse(Q7171bitAnd2bitCharacters.isOneBitCharacter(new int[]{1, 1, 1, 0}));
+    }
+}

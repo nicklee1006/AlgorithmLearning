d36cc61a61e9ac723095ad10b431eab8ce3544ee
nick lee <nicklee1006@gmail.com>
2018-07-04 19:51:56 +0930

    add Question 728. Self Dividing Numbers

---
+++ test/easy/Q728SelfDividingNumbersTest.java
@@ -0,0 +1,13 @@
+package easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q728SelfDividingNumbersTest {
+
+    @Test
+    public void selfDividingNumbers() {
+        Q728SelfDividingNumbers.selfDividingNumbers(20, 22);
+    }
+}

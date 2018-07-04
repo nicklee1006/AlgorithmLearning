47858e4d4e76a68fa980c1fa6d5d825f7d887ae6
nick lee <nicklee1006@gmail.com>
2018-07-04 19:40:21 +0930

    add Question 724. Find Pivot Index

---
+++ test/easy/Q724FindPivotIndexTest.java
@@ -0,0 +1,15 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q724FindPivotIndexTest {
+
+    @Test
+    public void pivotIndex() {
+        Assert.assertEquals(3, Q724FindPivotIndex.pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
+        Assert.assertEquals(-1, Q724FindPivotIndex.pivotIndex(new int[]{1, 2, 3}));
+    }
+}

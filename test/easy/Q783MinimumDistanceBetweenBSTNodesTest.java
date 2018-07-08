53da5e44edc28d65e64598edee2ec5248c6acff1
nick lee <nicklee1006@gmail.com>
2018-07-08 21:55:20 +0930

    add Question 783. Minimum Distance Between BST Nodes

---
+++ test/easy/Q783MinimumDistanceBetweenBSTNodesTest.java
@@ -0,0 +1,12 @@
+package easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q783MinimumDistanceBetweenBSTNodesTest {
+
+    @Test
+    public void minDiffInBST() {
+    }
+}

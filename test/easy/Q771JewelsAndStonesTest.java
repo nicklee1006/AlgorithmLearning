ce97e2f16c926f0e0d822dc62b0aa7edeb74cee5
nick lee <nicklee1006@gmail.com>
2018-07-08 21:55:03 +0930

    add Question 771. Jewels and Stones

---
+++ test/easy/Q771JewelsAndStonesTest.java
@@ -0,0 +1,15 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q771JewelsAndStonesTest {
+
+    @Test
+    public void numJewelsInStones() {
+        Assert.assertEquals(3, Q771JewelsAndStones.numJewelsInStones("aA", "aAAbbbb"));
+        Assert.assertEquals(0, Q771JewelsAndStones.numJewelsInStones("z", "ZZ"));
+    }
+}

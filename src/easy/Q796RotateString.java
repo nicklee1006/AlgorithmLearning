cc0080acf71d85bd206538a956de18103f17b88f
nick lee <nicklee1006@gmail.com>
2018-07-10 13:45:22 +0930

    add Question 796. Rotate String

---
+++ src/easy/Q796RotateString.java
@@ -0,0 +1,17 @@
+package easy;
+
+/**
+ * 2018.7.10 Question 796. Rotate String
+ * https://leetcode.com/problems/rotate-string/description/
+ *
+ * We are given two strings, A and B.
+ *
+ * A shift on A consists of taking string A and moving the leftmost character to the rightmost
+ * position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return
+ * True if and only if A can become B after some number of shifts on A.
+ */
+public class Q796RotateString {
+    public static boolean rotateString(String A, String B) {
+        return A.length() == B.length() && (A + A).contains(B);
+    }
+}

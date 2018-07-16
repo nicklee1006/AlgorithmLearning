c4ccf392d18d79e4a6c0ee6c2452d7d335c1d82d
nick lee <nicklee1006@gmail.com>
2018-07-16 15:31:45 +0930

    add Question 830. Positions of Large Groups

---
+++ test/easy/Q830PositionsOfLargeGroupsTest.java
@@ -0,0 +1,17 @@
+package easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q830PositionsOfLargeGroupsTest {
+
+    @Test
+    public void largeGroupPositions() {
+//        Q830PositionsOfLargeGroups.largeGroupPositions("abbxxxxzzy");
+//        Q830PositionsOfLargeGroups.largeGroupPositions("abc");
+//        Q830PositionsOfLargeGroups.largeGroupPositions("abcdddeeeeaabbbcd");
+        Q830PositionsOfLargeGroups.largeGroupPositions("aaa");
+
+    }
+}

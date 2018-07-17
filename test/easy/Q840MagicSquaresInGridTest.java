e09911d857396fc1909311bbb59fb207b5020bc8
nick lee <nicklee1006@gmail.com>
2018-07-17 12:04:00 +0930

    add Question 840. Magic Squares In Grid

---
+++ test/easy/Q840MagicSquaresInGridTest.java
@@ -0,0 +1,12 @@
+package easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q840MagicSquaresInGridTest {
+
+    @Test
+    public void numMagicSquaresInside() {
+    }
+}

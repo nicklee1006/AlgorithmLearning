a8e9cd5f4e20cbfa4f2a3a1f7122ab8fcf3c7661
nick lee <nicklee1006@gmail.com>
2018-07-22 16:00:36 +0930

    add Question 868. Binary Gap

---
+++ test/easy/Q868BinaryGapTest.java
@@ -0,0 +1,17 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q868BinaryGapTest {
+
+    @Test
+    public void binaryGap() {
+        Assert.assertEquals(2, Q868BinaryGap.binaryGap(22));
+        Assert.assertEquals(2, Q868BinaryGap.binaryGap(5));
+        Assert.assertEquals(1, Q868BinaryGap.binaryGap(6));
+        Assert.assertEquals(0, Q868BinaryGap.binaryGap(8));
+    }
+}

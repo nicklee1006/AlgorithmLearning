5767ab3c73ebcad8c426df05deee158e5de14189
nick lee <nicklee1006@gmail.com>
2018-10-16 13:43:21 +1030

    add Question 883. Projection Area of 3D Shapes

---
+++ test/easy/Q883ProjectionAreaOf3DShapesTest.java
@@ -0,0 +1,14 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Q883ProjectionAreaOf3DShapesTest {
+
+    @Test
+    public void projectionArea() {
+        Assert.assertEquals(5, Q883ProjectionAreaOf3DShapes.projectionArea(new int[][]{new int[]{2}}));
+        Assert.assertEquals(17, Q883ProjectionAreaOf3DShapes.projectionArea(new int[][]{new int[]{1,2}, new int[]{3,4}}));
+
+    }
+}

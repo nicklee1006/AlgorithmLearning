231dc417c79da55c52dfa30db11c9a5410c25e08
nick lee <nicklee1006@gmail.com>
2018-10-26 17:03:36 +1030

    add Question 914. X of a Kind in a Deck of Cards

---
+++ test/easy/Q914XOfAKindInADeckOfCardsTest.java
@@ -0,0 +1,19 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q914XOfAKindInADeckOfCardsTest {
+
+    @Test
+    public void hasGroupsSizeX() {
+        Assert.assertTrue(Q914XOfAKindInADeckOfCards.hasGroupsSizeX(new int[]{1,2,3,4,4,3,2,1}));
+        Assert.assertFalse(Q914XOfAKindInADeckOfCards.hasGroupsSizeX(new int[]{1,1,1,2,2,2,3,3}));
+        Assert.assertFalse(Q914XOfAKindInADeckOfCards.hasGroupsSizeX(new int[]{1}));
+        Assert.assertTrue(Q914XOfAKindInADeckOfCards.hasGroupsSizeX(new int[]{1,1}));
+        Assert.assertTrue(Q914XOfAKindInADeckOfCards.hasGroupsSizeX(new int[]{1,1,2,2,2,2}));
+        Assert.assertTrue(Q914XOfAKindInADeckOfCards.hasGroupsSizeX(new int[]{1,1,1,1,2,2,2,2,2,2}));
+    }
+}

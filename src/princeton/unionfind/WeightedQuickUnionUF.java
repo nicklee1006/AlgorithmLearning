dd2f72ff771e75a87746ca36bc217755cf30944b
nick lee <nicklee1006@gmail.com>
2019-06-29 22:47:31 +1000

    Coursera - Princeton Algorithm week 1 union find

---
+++ src/princeton/unionfind/WeightedQuickUnionUF.java
@@ -0,0 +1,55 @@
+package princeton.unionfind;
+
+public class WeightedQuickUnionUF implements UnionFind {
+    private int[] id;
+    private int[] size;
+
+    public WeightedQuickUnionUF(int N) {
+        id = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    @Override
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+
+        if (i == j) {
+            return;
+        }
+
+        if (size[i] < size[j]) {
+            id[i] = j;
+            size[j] += size[i];
+        } else {
+            id[j] = i;
+            size[i] += size[j];
+        }
+    }
+
+    @Override
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    @Override
+    public int find(int p) {
+        return 0;
+    }
+
+    @Override
+    public int countComponent() {
+        return 0;
+    }
+
+    private int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+
+        return i;
+    }
+}

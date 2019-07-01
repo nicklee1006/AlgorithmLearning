b3df36fe8eed5cb7e947d730398670d296c634ed
nick lee <nicklee1006@gmail.com>
2019-07-02 00:15:52 +1000

    LeetCode 7. Reverse Integer - Runtime: 1ms/100%; Memory: 33.7 MB/14.75%

--- src/leetcode/easy/Q7ReverseInteger.java
+++ src/leetcode/easy/Q7ReverseInteger.java
@@ -1,67 +1,97 @@
 package leetcode.easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 2018.3.16 Question 7: Reverse Integer
  * https://leetcode.com/problems/reverse-integer/description/
  *
  * Given a 32-bit signed integer, reverse digits of an integer.
- *
- * 1. check if it is < 0
- *  1.1 if < 0, remove '-', keep a record
- * 2. convert to string, and reverse it
- * 3. remove '0' at the beginning
- * 4. add back '-' if necessary
- * 5. check if it > MAX_VALUE or < MIN_VALUE
  */
 public class Q7ReverseInteger {
     public static int reverse(int x) {
-        String numberString = String.valueOf(x);
+        Queue<Integer> queue = new LinkedList<>();
 
-        // process negative
-        if (numberString.startsWith("-")) {
-            numberString = numberString.substring(1, numberString.length());
+        // push each digit to stack
+        while (x != 0) {
+            queue.offer(x % 10);
 
-            StringBuilder builder = new StringBuilder();
-            builder.append(numberString);
-            builder.reverse();
-
-            numberString = builder.toString();
-
-            while (numberString.startsWith("0")) {
-                numberString = numberString.substring(1, numberString.length());
-            }
-
-            numberString = "-" + numberString;
-
-            long result = Long.parseLong(numberString);
-
-            if (result >= Integer.MAX_VALUE || result <= Integer.MIN_VALUE) {
-                return 0;
-            } else {
-                return (int) result;
-            }
-        } else {
-            StringBuilder builder = new StringBuilder();
-            builder.append(numberString);
-            builder.reverse();
-
-            numberString = builder.toString();
-
-            while (numberString.startsWith("0")) {
-                numberString = numberString.substring(1, numberString.length());
-            }
-
-            if (numberString.equals("")) {
-                numberString = "0";
-            }
-
-            long result = Long.parseLong(numberString);
-
-            if (result >= Integer.MAX_VALUE || result <= Integer.MIN_VALUE) {
-                return 0;
-            } else {
-                return (int) result;
-            }
+            x = x / 10;
         }
+
+        // Restore digit
+        int result = 0;
+        while (!queue.isEmpty()) {
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && queue.peek() > 7)) {
+                return 0;
+            }
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && queue.peek() < -8)) {
+                return 0;
+            }
+
+            result = result * 10 + queue.poll();
+        }
+
+        return result;
     }
+
+    /**
+     * 1. check if it is < 0
+     *  1.1 if < 0, remove '-', keep a record
+     * 2. convert to string, and reverse it
+     * 3. remove '0' at the beginning
+     * 4. add back '-' if necessary
+     * 5. check if it > MAX_VALUE or < MIN_VALUE
+     */
+//    public static int reverse(int x) {
+//        String numberString = String.valueOf(x);
+//
+//        // process negative
+//        if (numberString.startsWith("-")) {
+//            numberString = numberString.substring(1);
+//
+//            StringBuilder builder = new StringBuilder();
+//            builder.append(numberString);
+//            builder.reverse();
+//
+//            numberString = builder.toString();
+//
+//            while (numberString.startsWith("0")) {
+//                numberString = numberString.substring(1);
+//            }
+//
+//            numberString = "-" + numberString;
+//
+//            long result = Long.parseLong(numberString);
+//
+//            if (result >= Integer.MAX_VALUE || result <= Integer.MIN_VALUE) {
+//                return 0;
+//            } else {
+//                return (int) result;
+//            }
+//        } else {
+//            StringBuilder builder = new StringBuilder();
+//            builder.append(numberString);
+//            builder.reverse();
+//
+//            numberString = builder.toString();
+//
+//            while (numberString.startsWith("0")) {
+//                numberString = numberString.substring(1);
+//            }
+//
+//            if (numberString.equals("")) {
+//                numberString = "0";
+//            }
+//
+//            long result = Long.parseLong(numberString);
+//
+//            if (result >= Integer.MAX_VALUE || result <= Integer.MIN_VALUE) {
+//                return 0;
+//            } else {
+//                return (int) result;
+//            }
+//        }
+//    }
 }
--- test/leetcode/easy/Q7ReverseIntegerTest.java
+++ test/leetcode/easy/Q7ReverseIntegerTest.java
@@ -10,5 +10,7 @@ public class Q7ReverseIntegerTest {
         Assert.assertEquals(321, Q7ReverseInteger.reverse(123));
         Assert.assertEquals(-321,Q7ReverseInteger.reverse(-123));
         Assert.assertEquals(21,Q7ReverseInteger.reverse(120));
+        Assert.assertEquals(0,Q7ReverseInteger.reverse(1534236469));
+        Assert.assertEquals(-2143847412,Q7ReverseInteger.reverse(-2147483412));
     }
 }

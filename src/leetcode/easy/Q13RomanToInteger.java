b4f905b20a4853861171bb73654014936f5bdbd3
nick lee <nicklee1006@gmail.com>
2019-07-03 23:00:53 +1000

    LeetCode 13. Roman to Integer - 6 ms/53.02%, 36.7 MB/99.93%

--- src/leetcode/easy/Q13RomanToInteger.java
+++ src/leetcode/easy/Q13RomanToInteger.java
@@ -1,5 +1,10 @@
 package leetcode.easy;
 
+import edu.princeton.cs.algs4.In;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 2018.3.18 Question 13: Roman to Integer
  * https://leetcode.com/problems/roman-to-integer
@@ -29,54 +34,92 @@ package leetcode.easy;
  * 3999.
  */
 public class Q13RomanToInteger {
+    /**
+     * Remove use of string.contains
+     */
     public static int romanToInt(String s) {
+        Map<Character, Integer> numberMap = new HashMap<>();
+        numberMap.put('I', 1);
+        numberMap.put('V', 5);
+        numberMap.put('X', 10);
+        numberMap.put('L', 50);
+        numberMap.put('C', 100);
+        numberMap.put('D', 500);
+        numberMap.put('M', 1000);
+
         int result = 0;
 
-        if (s.contains("IV")) {
-            result -= 2;
-        }
-        if (s.contains("IX")) {
-            result -= 2;
-        }
-        if (s.contains("XL")) {
-            result -= 20;
-        }
-        if (s.contains("XC")) {
-            result -= 20;
-        }
-        if (s.contains("CD")) {
-            result -= 200;
-        }
-        if (s.contains("CM")) {
-            result -= 200;
-        }
+        // need to compare with previous value
+        int prev = 0;
+        int current;
 
         for (int i = 0; i < s.length(); i++) {
-            char temp = s.charAt(i);
+            current = numberMap.get(s.charAt(i));
 
-            if (temp == 'I') {
-                result += 1;
+
+            if (current <= prev) {
+                result += current;
+            } else {
+                result += current - 2 * prev;
             }
-            if (temp == 'V') {
-                result += 5;
-            }
-            if (temp == 'X') {
-                result += 10;
-            }
-            if (temp == 'L') {
-                result += 50;
-            }
-            if (temp == 'C') {
-                result += 100;
-            }
-            if (temp == 'D') {
-                result += 500;
-            }
-            if (temp == 'M') {
-                result += 1000;
-            }
+
+            prev = current;
         }
 
         return result;
     }
+
+    /**
+     * Just follow the rules
+     */
+//    public static int romanToInt(String s) {
+//        int result = 0;
+//
+//        if (s.contains("IV")) {
+//            result -= 2;
+//        }
+//        if (s.contains("IX")) {
+//            result -= 2;
+//        }
+//        if (s.contains("XL")) {
+//            result -= 20;
+//        }
+//        if (s.contains("XC")) {
+//            result -= 20;
+//        }
+//        if (s.contains("CD")) {
+//            result -= 200;
+//        }
+//        if (s.contains("CM")) {
+//            result -= 200;
+//        }
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            char temp = s.charAt(i);
+//
+//            if (temp == 'I') {
+//                result += 1;
+//            }
+//            if (temp == 'V') {
+//                result += 5;
+//            }
+//            if (temp == 'X') {
+//                result += 10;
+//            }
+//            if (temp == 'L') {
+//                result += 50;
+//            }
+//            if (temp == 'C') {
+//                result += 100;
+//            }
+//            if (temp == 'D') {
+//                result += 500;
+//            }
+//            if (temp == 'M') {
+//                result += 1000;
+//            }
+//        }
+//
+//        return result;
+//    }
 }

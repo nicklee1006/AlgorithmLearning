9e314831bf40323d71d5cfd4280cc47f7b7292a6
nick lee <nicklee1006@gmail.com>
2019-07-07 23:55:49 +1000

    LeetCode 14. Longest Common Prefix - 0 ms/100%, 37.4 MB/95.26%

--- test/leetcode/easy/Q14LongestCommonPrefixTest.java
+++ test/leetcode/easy/Q14LongestCommonPrefixTest.java
@@ -9,5 +9,11 @@ public class Q14LongestCommonPrefixTest {
     public void longestCommonPrefix() {
         String[] testCase = {"test123", "test1", "test111"};
         Assert.assertEquals("test1", Q14LongestCommonPrefix.longestCommonPrefix(testCase));
+
+        String[] testCase2 = {"flower", "flow", "flight"};
+        Assert.assertEquals("fl", Q14LongestCommonPrefix.longestCommonPrefix(testCase2));
+
+        String[] testCase3 = {"dog","racecar","car"};
+        Assert.assertEquals("", Q14LongestCommonPrefix.longestCommonPrefix(testCase3));
     }
 }

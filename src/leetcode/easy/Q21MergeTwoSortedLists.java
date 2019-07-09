4a0a68eff7242e9c11a40be6baeb2f5fdc69fa54
nick lee <nicklee1006@gmail.com>
2019-07-09 21:50:44 +1000

    LeetCode 21. Merge Two Sorted Lists - 0ms/100%, 39.3MB/15.9%

--- src/leetcode/easy/Q21MergeTwoSortedLists.java
+++ src/leetcode/easy/Q21MergeTwoSortedLists.java
@@ -9,14 +9,16 @@ import leetcode.util.ListNode;
  * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing
  * together the nodes of the first two lists.
  *
- * Solution:
- * 1. check if l1 or l2 is empty
- * 2. while l1 and l2 are not null
- *  2.1 compare value of l1 and l2, give smaller value to new list
- *  2.2 move smaller value list to next node
- * 3. connect the list that is not empty to the end of new list
  */
 public class Q21MergeTwoSortedLists {
+    /**
+     * * Solution:
+     *  1. check if l1 or l2 is empty
+     *  2. while l1 and l2 are not null
+     *     2.1 compare value of l1 and l2, give smaller value to new list
+     *     2.2 move smaller value list to next node
+     *  3. connect the list that is not empty to the end of new list
+     */
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
@@ -26,27 +28,23 @@ public class Q21MergeTwoSortedLists {
             return l1;
         }
 
-        ListNode result = new ListNode(0);
-        ListNode head = result;
+        ListNode dummyNode = new ListNode(0);
+        ListNode node = dummyNode;
 
         while (l1 != null && l2 != null) {
             if (l1.val <= l2.val) {
-                head.next = new ListNode(l1.val);
+                node.next = new ListNode(l1.val);
                 l1 = l1.next;
             } else {
-                head.next = new ListNode(l2.val);
+                node.next = new ListNode(l2.val);
                 l2 = l2.next;
             }
-            head = head.next;
+
+            node = node.next;
         }
 
-        if (l1 == null) {
-            head.next = l2;
-        }
-        if (l2 == null) {
-            head.next = l1;
-        }
+        node.next = l1 == null ? l2 : l1;
 
-        return result.next;
+        return dummyNode.next;
     }
 }

fd4e0d46b7d200e32a8e209973d4ad1b9df37ea3
nick lee <nicklee1006@gmail.com>
2019-07-10 23:02:53 +1000

    Coursera - Princeton Algorithm week 2 elementary sorts

---
+++ src/princeton/w2_elementarysorts/ElementarySort.java
@@ -0,0 +1,15 @@
+package princeton.w2_elementarysorts;
+
+public abstract class ElementarySort {
+    public abstract void sort(Comparable[] a);
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void exchange(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+}
---
+++ src/princeton/w2_elementarysorts/SelectionSort.java
@@ -0,0 +1,18 @@
+package princeton.w2_elementarysorts;
+
+public class SelectionSort extends ElementarySort {
+    @Override
+    public void sort(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < a.length; j++) {
+                if (less(a[j], a[minIndex])) {
+                    minIndex = j;
+                }
+
+                exchange(a, i, minIndex);
+            }
+        }
+    }
+}

114f5ce9ba78026c09657833dbf5761c623fa511
nick lee <nicklee1006@gmail.com>
2019-07-13 16:50:43 +1000

    Coursera - Princeton Algorithm week 3 quicksorts

---
+++ src/princeton/w3_quicksort/ThreeWayPartition.java
@@ -0,0 +1,38 @@
+package princeton.w3_quicksort;
+
+public class ThreeWayPartition {
+    public static void sort(Comparable[] a, int low, int hi) {
+        if (low >= hi) {
+            return;
+        }
+
+        int i = low;
+        int lt = low;
+        int gt = hi;
+
+        Comparable key = a[low];
+
+        while (i <= gt) {
+            if (less(a[i], key)) {
+                exchange(a, lt++, i++);
+            } else if (less(key, a[i])) {
+                exchange(a, gt--, i);
+            } else {
+                i++;
+            }
+        }
+
+        sort(a, low, lt - 1);
+        sort(a, gt + 1, hi);
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exchange(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+}

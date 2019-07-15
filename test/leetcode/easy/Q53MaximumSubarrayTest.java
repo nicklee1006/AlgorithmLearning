a3d57a321310c04384b385f8c16e24ddeb787499
nick lee <nicklee1006@gmail.com>
2019-07-15 22:25:45 +1000

    LeetCode 53. Maximum Subarray - 0ms/100%, 37.9MB/97.65%
    Signed-off-by: nick lee <nicklee1006@gmail.com>

--- test/leetcode/easy/Q53MaximumSubarrayTest.java
+++ test/leetcode/easy/Q53MaximumSubarrayTest.java
@@ -8,5 +8,7 @@ public class Q53MaximumSubarrayTest {
     @Test
     public void maxSubArray() {
         Assert.assertEquals(6 ,Q53MaximumSubarray.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        Assert.assertEquals(1 ,Q53MaximumSubarray.maxSubArray(new int[]{1}));
+
     }
 }

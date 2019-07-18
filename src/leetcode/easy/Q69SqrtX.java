625d1731413864556ab9342d73395052ddd76f19
nick lee <nicklee1006@gmail.com>
2019-07-18 21:00:53 +1000

    LeetCode 69. Sqrt(x) - 1ms/100%, 33.6MB/5%
    Signed-off-by: nick lee <nicklee1006@gmail.com>

--- src/leetcode/easy/Q69SqrtX.java
+++ src/leetcode/easy/Q69SqrtX.java
@@ -7,19 +7,49 @@ package leetcode.easy;
  * Compute and return the square root of x.
  * x is guaranteed to be a non-negative integer.
  *
- * Solution:
- * 1. result <= x/result
  */
 public class Q69SqrtX {
+    /**
+     * Do a binary search from 1 to x
+     */
     public static int mySqrt(int x) {
-        int result = 1;
+        int start = 1;
+        int end = x;
 
-        while (result <= x/result) {
-            result++;
+        int result = 0;
+
+        while (start <= end) {
+            int mid = end - (end - start) / 2;
+
+            if (mid == x / mid) {
+                return mid;
+            }
+
+            if (mid > x / mid) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+                result = mid;
+            }
         }
-
-        result--;
 
         return result;
     }
+
+
+    /**
+     *  Solution:
+     *  1. result <= x/result
+     */
+//    public static int mySqrt(int x) {
+//        int result = 1;
+//
+//        while (result <= x / result) {
+//            result++;
+//        }
+//
+//        result--;
+//
+//        return result;
+//    }
 }

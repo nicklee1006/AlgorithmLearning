53e1b9a3374c8c5056a1112e4cd8e4cc5bd24e41
nick lee <nicklee1006@gmail.com>
2019-07-30 23:46:50 +1000

    Add utils to test LinkedList
    Signed-off-by: nick lee <nicklee1006@gmail.com>

---
+++ test/util/LinkedListTestUtil.java
@@ -0,0 +1,36 @@
+package util;
+
+import leetcode.util.ListNode;
+
+public class LinkedListTestUtil {
+    public static ListNode createLinkedListFromArray(int[] array) {
+        if (array.length == 0) {
+            return null;
+        }
+
+        ListNode head = new ListNode(array[0]);
+        ListNode previous = head;
+
+        for (int i = 1; i < array.length; i++) {
+            ListNode node = new ListNode(array[i]);
+
+            previous.next = node;
+            previous = node;
+        }
+
+        return head;
+    }
+
+    public static boolean isTwoLinkedListEqual(ListNode list1, ListNode list2) {
+        while (list1 != null && list2 != null) {
+            if (list1.val != list2.val) {
+                return false;
+            }
+
+            list1 = list1.next;
+            list2 = list2.next;
+        }
+
+        return list1 == null && list2 == null;
+    }
+}

e2f7c5c37b615581c8b9dbe5402e1361124182f3
nick lee <nicklee1006@gmail.com>
2019-08-11 20:08:26 +1000

    LeetCode 40. Combination Sum II - 4ms/66%, 36MB/100%
    Signed-off-by: nick lee <nicklee1006@gmail.com>

--- src/main/java/leetcode/medium/Q40CombinationSumII.java
+++ src/main/java/leetcode/medium/Q40CombinationSumII.java
@@ -1,12 +1,16 @@
 package leetcode.medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Q40CombinationSumII {
     public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> results = new ArrayList<>();
+
+        // sort to remove duplicates
+        Arrays.sort(candidates);
 
         backtrack(results, new ArrayList<>(), candidates, target, 0);
 
@@ -20,17 +24,22 @@ public class Q40CombinationSumII {
         if (remaining < 0) {
             return;
         } else if (remaining == 0) {
-            List<Integer> tempResult = new ArrayList<>(tempList);
+//            List<Integer> tempResult = new ArrayList<>(tempList);
+//
+//            for (List<Integer> result : results) {
+//                if (result.size() == tempResult.size() && twoListEquals(result, tempResult)) {
+//                    return;
+//                }
+//            }
 
-            for (List<Integer> result : results) {
-                if (result.size() == tempResult.size() && twoListEquals(result, tempResult)) {
-                    return;
-                }
-            }
-
-            results.add(tempResult);
+            results.add(new ArrayList<>(tempList));
         } else {
             for (int i = start; i < candidates.length; i++) {
+                // skip same number
+                if (i > start && candidates[i] == candidates[i - 1]) {
+                    continue;
+                }
+
                 tempList.add(candidates[i]);
 
                 backtrack(results, tempList, candidates, remaining - candidates[i], i + 1);
@@ -42,17 +51,17 @@ public class Q40CombinationSumII {
     }
 
     // assume same size
-    private static boolean twoListEquals(List<Integer> list1, List<Integer> list2) {
-        List<Integer> list1Copy = new ArrayList<>(list1);
-
-        for (Integer integer : list2) {
-            if (!list1Copy.contains(integer)) {
-                return false;
-            } else {
-                list1Copy.remove(integer);
-            }
-        }
-
-        return true;
-    }
+//    private static boolean twoListEquals(List<Integer> list1, List<Integer> list2) {
+//        List<Integer> list1Copy = new ArrayList<>(list1);
+//
+//        for (Integer integer : list2) {
+//            if (!list1Copy.contains(integer)) {
+//                return false;
+//            } else {
+//                list1Copy.remove(integer);
+//            }
+//        }
+//
+//        return true;
+//    }
 }

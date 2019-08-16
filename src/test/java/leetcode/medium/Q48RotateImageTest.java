2d854c80afe105130d6dd6c8c27cb945e6d4c2ae
nick lee <nicklee1006@gmail.com>
2019-08-16 23:17:44 +1000

    LeetCode 48. Rotate Image - 0ms/100%, 36MB/100%
    Signed-off-by: nick lee <nicklee1006@gmail.com>

---
+++ src/test/java/leetcode/medium/Q48RotateImageTest.java
@@ -0,0 +1,28 @@
+package leetcode.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class Q48RotateImageTest {
+
+    @Test
+    public void rotate() {
+        int[][] input = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int[][] expected = new int[][]{
+                {7, 4, 1},
+                {8, 5, 2},
+                {9, 6, 3}
+        };
+
+        Q48RotateImage.rotate(input);
+
+        Assert.assertTrue(Arrays.deepEquals(input, expected));
+    }
+}

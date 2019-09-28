0c93b26825bd8733a7e783bb47baa1efc9c34564
nick lee <nicklee1006@gmail.com>
2019-09-28 22:40:52 +1000

    LeetCode 101. Symmetric Tree update iterative- 1ms/44%, 39MB/40%
    Signed-off-by: nick lee <nicklee1006@gmail.com>

--- Leetcode/src/main/java/easy/Q101SymmetricTree.java
+++ Leetcode/src/main/java/easy/Q101SymmetricTree.java
@@ -2,6 +2,8 @@ package easy;
 
 import util.TreeNode;
 
+import java.util.*;
+
 /**
  * 2018.4.6 Symmetric Tree
  * https://leetcode.com/problems/symmetric-tree/description/
@@ -19,18 +21,53 @@ public class Q101SymmetricTree {
             return true;
         }
 
-        return isSymmetric(root.left, root.right);
-    }
+        Queue<TreeNode> leftTree = new LinkedList<>();
+        Queue<TreeNode> rightTree = new LinkedList<>();
 
-    private static boolean isSymmetric(TreeNode left, TreeNode right) {
-        if (left == null && right == null) {
-            return true;
+        leftTree.offer(root.left);
+        rightTree.offer(root.right);
+
+        while (!leftTree.isEmpty() && !rightTree.isEmpty()) {
+            TreeNode currentLeft = leftTree.poll();
+            TreeNode currentRight = rightTree.poll();
+
+            if ((currentLeft == null && currentRight != null) || (currentLeft != null && currentRight == null)) {
+                return false;
+            }
+
+            if (currentLeft != null && currentRight != null) {
+                if (currentLeft.val != currentRight.val) {
+                    return false;
+                }
+
+                leftTree.offer(currentLeft.left);
+                leftTree.offer(currentLeft.right);
+
+                rightTree.offer(currentRight.right);
+                rightTree.offer(currentRight.left);
+            }
         }
 
-        if (left == null || right == null || left.val != right.val) {
-            return false;
-        }
-
-        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        return leftTree.isEmpty() && rightTree.isEmpty();
     }
+
+//    public static boolean isSymmetric(TreeNode root) {
+//        if (root == null) {
+//            return true;
+//        }
+//
+//        return isSymmetric(root.left, root.right);
+//    }
+//
+//    private static boolean isSymmetric(TreeNode left, TreeNode right) {
+//        if (left == null && right == null) {
+//            return true;
+//        }
+//
+//        if (left == null || right == null || left.val != right.val) {
+//            return false;
+//        }
+//
+//        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+//    }
 }
--- Leetcode/src/test/java/easy/Q101SymmetricTreeTest.java
+++ Leetcode/src/test/java/easy/Q101SymmetricTreeTest.java
@@ -2,27 +2,17 @@ package easy;
 
 import org.junit.Assert;
 import org.junit.Test;
+import testutil.TreeTestUtil;
 import util.TreeNode;
 
 public class Q101SymmetricTreeTest {
 
     @Test
     public void isSymmetric() {
-        TreeNode treeNode1 = new TreeNode(1);
-        TreeNode treeNode2 = new TreeNode(2);
-        TreeNode treeNode3 = new TreeNode(2);
-        TreeNode treeNode4 = new TreeNode(3);
-        TreeNode treeNode5 = new TreeNode(4);
-        TreeNode treeNode6 = new TreeNode(3);
-        TreeNode treeNode7 = new TreeNode(4);
+        TreeNode input1 = TreeTestUtil.createBinaryTreeFromArray(new Integer[]{1,2,2,3,4,4,3});
+        TreeNode input2 = TreeTestUtil.createBinaryTreeFromArray(new Integer[]{1,2,2,null,3,null,3});
 
-        treeNode1.left = treeNode2;
-        treeNode1.right = treeNode3;
-        treeNode2.left = treeNode4;
-        treeNode2.right = treeNode5;
-        treeNode3.left = treeNode6;
-        treeNode3.right = treeNode7;
-
-        Assert.assertTrue(Q101SymmetricTree.isSymmetric(treeNode1));
+        Assert.assertTrue(Q101SymmetricTree.isSymmetric(input1));
+        Assert.assertFalse(Q101SymmetricTree.isSymmetric(input2));
     }
 }

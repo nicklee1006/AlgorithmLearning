55522b436244869834654dcc9035678036001635
nick lee <nicklee1006@gmail.com>
2019-10-12 15:06:22 +1000

    LeetCode 119. Pascal's Triangle II - 1ms/88%, 33MB/6%
    Signed-off-by: nick lee <nicklee1006@gmail.com>

--- Leetcode/src/main/java/easy/Q119PascalTriangleII.java
+++ Leetcode/src/main/java/easy/Q119PascalTriangleII.java
@@ -24,7 +24,7 @@ public class Q119PascalTriangleII {
         for (int i = 0; i < rowIndex + 1; i++) {
             result.add(0, 1);
 
-            for (int j = 1; j < result.size()-1; j++) {
+            for (int j = 1; j < result.size() - 1; j++) {
                 result.set(j, result.get(j) + result.get(j + 1));
             }
         }
--- Leetcode/src/test/java/easy/Q119PascalTriangleIITest.java
+++ Leetcode/src/test/java/easy/Q119PascalTriangleIITest.java
@@ -1,15 +1,20 @@
 package easy;
 
-import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 public class Q119PascalTriangleIITest {
 
     @Test
     public void getRow() {
+        List<Integer> expected = Arrays.asList(1, 3, 3, 1);
+
         List<Integer> result = Q119PascalTriangleII.getRow(3);
-        Assert.assertArrayEquals(new int[]{1,3,3,1}, result.stream().mapToInt(Integer::intValue).toArray());
+
+        assertTrue(expected.size() == result.size() && expected.containsAll(result));
     }
 }

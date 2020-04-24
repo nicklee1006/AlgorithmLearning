caa19cd08d2fc63baa4470c489f2a0b5bbcdafda
nick lee <nicklee1006@gmail.com>
2020-04-24 14:01:41 +1000

    optimize LeetCode 142 Linked List Cycle II
    Signed-off-by: nick lee <nicklee1006@gmail.com>

--- Leetcode/src/main/java/medium/Q142LinkedListCycleII.java
+++ Leetcode/src/main/java/medium/Q142LinkedListCycleII.java
@@ -7,18 +7,50 @@ import util.ListNode;
 
 public class Q142LinkedListCycleII {
     public static ListNode detectCycle(ListNode head) {
-        Set<ListNode> nodes = new HashSet<>();
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode meet = null;
 
-        while (head != null) {
-            nodes.add(head);
-
-            head = head.next;
-
-            if (nodes.contains(head)) {
-                return head;
+        while (fast != null) {
+            // no loop
+            if (fast.next == null) {
+                return null;
             }
+        }
+
+        // move forward slow and fast
+        slow = slow.next;
+        fast = fast.next.next;
+
+        // reach the meet point
+        if (fast == slow) {
+            meet = fast;
+
+            // start move forward head, until meet point
+            while (head != meet) {
+                head = head.next;
+                meet = meet.next;
+            }
+
+            return head;
         }
 
         return null;
     }
+
+//    public static ListNode detectCycle(ListNode head) {
+//        Set<ListNode> nodes = new HashSet<>();
+//
+//        while (head != null) {
+//            nodes.add(head);
+//
+//            head = head.next;
+//
+//            if (nodes.contains(head)) {
+//                return head;
+//            }
+//        }
+//
+//        return null;
+//    }
 }
